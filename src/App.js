58bfd80252eae2392be088c7e7460576f9748f4d
Arslan Sami <arslansami810@gmail.com>
2025-01-04 14:22:39 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+// Example App.js:
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Soar Task</h1>
+      </header>
+    </div>
+  );
+}
